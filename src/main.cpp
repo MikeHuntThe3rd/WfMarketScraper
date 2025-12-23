544f03ce7f949143184f87a078ddd273861545eb
MikeHuntThe3rd <103815901+MikeHuntThe3rd@users.noreply.github.com>
2025-12-23 21:49:28 +0100

    compiles on linux now and has handling for parameterless running

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,29 @@
 #include "Sorting.hpp"
 #include "OrderHandling.hpp"
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
     CurlReq::setup();
     std::cout << "runs" << std::endl;
-
-    std::ofstream del("out.log", std::ios::trunc);
-    del.close();
-    OrderHandling::UpdateOrder(argv[1], "6940730fe2f5699c2150da0d", itemType::mod, tradeType::buy, rank{1,1,1});
-    OrderHandling::DeleteOrder(argv[1]);
-    json items = CurlReq::GETjson("https://api.warframe.market/v2/items", {"accept: application/json", "Language: en"});
-    for(json item: items["data"]){
-        auto trade = Sorting::ValidTrade(item["slug"], item["tags"].get<std::vector<std::string>>(), true);
-        if(trade.good_trade){
-            std::string id = item["id"].get<std::string>();
-            OrderHandling::PostOrder(argv[1], id, tradeType::buy, trade);
-        }
+    if(argc == 1){
+	std::cout << "succesfull compalation, no parameter" << std::endl;
     }
-    CurlReq::disconnect();
+    else {
+	std::cout << "succesfull compalation, parameter given" << std::endl;
+	std::ofstream del("out.log", std::ios::trunc);
+	del.close();
+	OrderHandling::UpdateOrder(argv[1], "6940730fe2f5699c2150da0d", itemType::mod, tradeType::buy, rank{1,1,1});
+	OrderHandling::DeleteOrder(argv[1]);
+	json items = CurlReq::GETjson("https://api.warframe.market/v2/items", {"accept: application/json", "Language: en"});
+	for(json item: items["data"]){
+	    auto trade = Sorting::ValidTrade(item["slug"], item["tags"].get<std::vector<std::string>>(), true);
+	    if(trade.good_trade){
+		std::string id = item["id"].get<std::string>();
+		OrderHandling::PostOrder(argv[1], id, tradeType::buy, trade);
+	    }
+	}
+	CurlReq::disconnect();
+    }
     return 0;
 }
