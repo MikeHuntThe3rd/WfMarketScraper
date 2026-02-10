5f4095797ae1df8d0f24bb81e127cfb5f3937dcc
MikeHuntThe3rd <103815901+MikeHuntThe3rd@users.noreply.github.com>
2026-02-10 21:42:52 +0100

    basic name changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include "CurlReq.hpp"
+#include "OrderHandling.hpp"
+#include "Sorting.hpp"
+#include "VARS.hpp"
+#include "json.hpp"
 #include <exception>
 #include <fstream>
 #include <iostream>
@@ -6,199 +11,202 @@
 #include <string>
 #include <thread>
 #include <vector>
-#include "json.hpp"
-#include "CurlReq.hpp"
-#include "Sorting.hpp"
-#include "OrderHandling.hpp"
-#include "VARS.hpp"
 
 struct MapElement {
-    std::string key;
-    int value;
+  std::string key;
+  int value;
 };
 
-std::vector<std::string> ParseArgv(int argc, char* argv[]){
-    std::vector<std::string> result(argv, argv + argc);
-    return result;
+std::vector<std::string> ParseArgv(int argc, char *argv[]) {
+  std::vector<std::string> result(argv, argv + argc);
+  return result;
 }
 
-void ChangeSettings(std::optional<MapElement> change = std::nullopt){
-    //set change if given
-    if(change.has_value()) VARS::Settings[change->key] = change->value;
-    //remake the entire json with the settings unordered map as a template
-    using json = nlohmann::json;
-    json def;
-    for(auto element : VARS::Settings){
-	def[element.first] = element.second;
+void ChangeSettings(std::optional<MapElement> change = std::nullopt) {
+  // set change if given
+  if (change.has_value())
+    VARS::Settings[change->key] = change->value;
+  // remake the entire json with the settings unordered map as a template
+  using json = nlohmann::json;
+  json def;
+  for (auto element : VARS::Settings) {
+    def[element.first] = element.second;
+  }
+  std::ofstream DefSettings("settings.json");
+  DefSettings << def;
+  DefSettings.close();
+}
+
+void CreateSettings() {
+  std::string fileName = "settings.json";
+  std::ifstream file(fileName.c_str());
+  if (!file.good()) {
+    std::cout << "settings generated with default settings" << std::endl;
+    ChangeSettings();
+  }
+}
+
+void SettingsSetup() {
+  using json = nlohmann::json;
+  CreateSettings();
+  std::ifstream in("settings.json");
+  json data = json::parse(in);
+  for (const auto &[key, value] : data.items()) {
+    auto setting = VARS::Settings.find(key);
+    if (setting != VARS::Settings.end()) {
+      try {
+        setting->second = value;
+      } catch (std::exception e) {
+        std::cout << e.what() << std::endl;
+      }
     }
-    std::ofstream DefSettings("settings.json");
-    DefSettings << def;
-    DefSettings.close();
+  }
 }
-
-void CreateSettings(){
-    std::string fileName = "settings.json";
-    std::ifstream file(fileName.c_str());
-    if(!file.good()){
-	std::cout << "settings generated with default settings" << std::endl;
-	ChangeSettings();
+// TODO: NEW FUNCTION TO CHECK IF TRADE EXISTS AND UPDATE IF IT DOES
+void WebLoop() {
+  CurlReq::setup();
+  std::ofstream del("out.log", std::ios::trunc);
+  del.close();
+  OrderHandling::DeleteOrder();
+  items = CurlReq::q
+              .Add([] {
+                return CurlReq::__GET("https://api.warframe.market/v2/items");
+              })
+              .get();
+  while (VARS::LogLoop) {
+    for (json item : items["data"]) {
+      auto trade = Sorting::ValidTrade(item["slug"], item["tags"],
+                                       VARS::Settings["-l"] != 0);
+      if (trade.good_trade) {
+        std::string id = (std::string)item["id"];
+        OrderHandling::PostOrder(id, tradeType::buy, trade);
+      }
     }
+  }
+  CurlReq::disconnect();
 }
 
-void SettingsSetup(){
-    using json = nlohmann::json;
+void Set(std::vector<std::string> args) {
+  if (args.size() > 4) {
+    std::cout << "too many arguments given" << std::endl;
+    return;
+  }
+  std::optional<std::string> mode =
+      (args.size() >= 3) ? std::optional<std::string>{(std::string)args[2]}
+                         : std::nullopt;
+  if (mode.has_value()) {
     CreateSettings();
-    std::ifstream in("settings.json");
-    json data = json::parse(in);
-    for(const auto & [key, value] : data.items()){
-	auto setting = VARS::Settings.find(key);
-	if(setting != VARS::Settings.end()){
-	    try {
-		setting->second = value;
-	    } 
-	    catch (std::exception e) {
-		std::cout << e.what() << std::endl;
-	    }
-	}
+    auto setting = VARS::Settings.find(mode.value());
+    int value;
+    try {
+      if (args.size() < 4) {
+        std::cout << "no value given" << std::endl;
+        return;
+      }
+      value = std::stoi(args[3]);
+    } catch (std::exception e) {
+      std::cout << "error: " << e.what() << std::endl;
+      return;
     }
-}
-//TODO: NEW FUNCTION TO CHECK IF TRADE EXISTS AND UPDATE IF IT DOES
-void WebLoop(){
-    CurlReq::setup();
-    std::ofstream del("out.log", std::ios::trunc);
-    del.close();
-    OrderHandling::DeleteOrder();
-    items = CurlReq::q.Add([]{ return CurlReq::__GET("https://api.warframe.market/v2/items");}).get();
-    while (VARS::LogLoop) {
-	for(json item: items["data"]){
-	    auto trade = Sorting::ValidTrade(item["slug"], item["tags"], VARS::Settings["-l"] != 0);
-	    if(trade.good_trade){
-			std::string id = item["id"].get<std::string>();
-			OrderHandling::PostOrder(id, tradeType::buy, trade);
-	    }
-	}
+    if (setting != VARS::Settings.end()) {
+      SettingsSetup();
+      ChangeSettings(MapElement{setting->first, value});
+      std::cout << "setting changed succesfully" << std::endl;
+    } else {
+      std::cout << "set mode not found" << std::endl;
+      return;
     }
-    CurlReq::disconnect();
+
+  } else {
+    std::cout << "no set mode given" << std::endl;
+  }
 }
 
-void Set(std::vector<std::string> args){
-    if(args.size() > 4){
-	std::cout << "too many arguments given" << std::endl;
-	return;
+void Run(std::vector<std::string> args) {
+  if (args.size() > 4) {
+    std::cout << "too many arguments given" << std::endl;
+    return;
+  }
+  if (args.size() == 4) {
+    std::string mode = args[2];
+    if (VARS::RunModes.find(mode) == VARS::RunModes.end()) {
+      std::cout << "mode not recognized" << std::endl;
+      return;
     }
-    std::optional<std::string> mode = (args.size() >= 3) ? std::optional<std::string>{(std::string)args[2]} : std::nullopt;
-    if(mode.has_value()){
-	CreateSettings();
-	auto setting = VARS::Settings.find(mode.value());
-	int value;
-	try {
-	    if(args.size() < 4){
-		std::cout << "no value given" << std::endl;
-		return;
-	    }
-	    value = std::stoi(args[3]);
-	} catch (std::exception e) {
-	    std::cout << "error: " << e.what() << std::endl;
-	    return;
-	}
-	if(setting != VARS::Settings.end()){
-	    SettingsSetup();
-	    ChangeSettings(MapElement{setting->first, value});
-	    std::cout << "setting changed succesfully" << std::endl;
-	}
-	else {
-	    std::cout << "set mode not found" << std::endl;
-	    return;
-	}
 
+    VARS::JWT = args[3];
+
+    if (mode == "-w") {
+      SettingsSetup();
+      WebLoop();
+    } else if (mode == "-d") {
+      CurlReq::setup();
+      OrderHandling::DeleteOrder();
+      CurlReq::q.WaitUntilQueueEmpty();
+      CurlReq::disconnect();
+    } else if (mode == "-l") {
+      CurlReq::setup();
+      items =
+          CurlReq::q
+              .Add([] {
+                return CurlReq::__GET("https://api.warframe.market/v2/items");
+              })
+              .get();
+      OrderHandling::EElogChecking();
+      CurlReq::disconnect();
     }
-    else {
-	std::cout << "no set mode given" << std::endl;
+  }
+  if (args.size() == 3) {
+    if (VARS::RunModes.find(args[2]) != VARS::RunModes.end()) {
+      std::cout << "invalid command. \nNOTE:all web operation require your jwt "
+                   "token in the last argument"
+                << std::endl;
+      return;
     }
+    VARS::JWT = args[2];
+    std::thread InGameTrades(OrderHandling::EElogChecking);
+    WebLoop();
+    InGameTrades.join();
+  }
 }
 
-void Run(std::vector<std::string> args){
-    if(args.size() > 4){
-		std::cout << "too many arguments given" << std::endl;
-		return;
-    }
-    if(args.size() == 4){
-    	std::string mode = args[2];
-		if(VARS::RunModes.find(mode) == VARS::RunModes.end()){
-			std::cout << "mode not recognized" << std::endl;
-			return;
-		}
-		
-		VARS::JWT = args[3];
-		
-		if(mode == "-w"){
-			SettingsSetup();
-			WebLoop();
-		}
-		else if (mode == "-d") {
-			CurlReq::setup();
-			OrderHandling::DeleteOrder();
-			CurlReq::q.WaitUntilQueueEmpty();
-			CurlReq::disconnect();
-		}
-		else if (mode == "-l") {
-			CurlReq::setup();
-			items = CurlReq::q.Add([]{ return CurlReq::__GET("https://api.warframe.market/v2/items");}).get();
-			OrderHandling::EElogChecking();
-			CurlReq::disconnect();
-		}
-    }
-    if(args.size() == 3) {
-		if(VARS::RunModes.find(args[2]) != VARS::RunModes.end()){
-			std::cout << "invalid command. \nNOTE:all web operation require your jwt token in the last argument" << std::endl;
-			return;
-		}
-		VARS::JWT = args[2];
-    	std::thread InGameTrades(OrderHandling::EElogChecking);
-		WebLoop();
-		InGameTrades.join();
-	}
+void CommandDispatch(std::vector<std::string> args) {
+  if (args.size() <= 2) {
+    std::cout << "too few arguments" << std::endl;
+    return;
+  }
+  if ((std::string)args[1] == "run") {
+    Run(args);
+  } else if ((std::string)args[1] == "set") {
+    Set(args);
+  } else {
+    std::cout << "command not recognized" << std::endl;
+  }
 }
 
-void CommandDispatch(std::vector<std::string> args){
-    if(args.size() <= 2){
-	std::cout << "too few arguments" << std::endl;
-	return;
-    }
-    if((std::string)args[1] == "run"){
-	Run(args);
-    }
-    else if ((std::string)args[1] == "set") {
-	Set(args);
-    }
-    else {
-	std::cout << "command not recognized" << std::endl;
-    }
-}
-
-int main(int argc, char* argv[]) {
-    CommandDispatch(ParseArgv(argc, argv));
-    /*
-    CurlReq::setup();
-    std::cout << "runs" << std::endl;
-    if(argc == 1){
-		std::cout << "succesfull compalation, no parameter" << std::endl;
-		std::thread InGameTrades(OrderHandling::EElogChecking);
-		InGameTrades.join();
-    }
-    else {
-	std::ofstream del("out.log", std::ios::trunc);
-	del.close();
-	OrderHandling::DeleteOrder(argv[1]);
-	json items = CurlReq::q.Add([]{ return CurlReq::__GET("https://api.warframe.market/v2/items");}).get();
-	for(json item: items["data"]){
-	    auto trade = Sorting::ValidTrade(item["slug"], item["tags"].get<std::vector<std::string>>(), true);
-	    if(trade.good_trade){
-		std::string id = item["id"].get<std::string>();
-		OrderHandling::PostOrder(argv[1], id, tradeType::buy, trade);
-	    }
-	}
-	CurlReq::disconnect();
-    }*/
-    return 0;
+int main(int argc, char *argv[]) {
+  CommandDispatch(ParseArgv(argc, argv));
+  /*
+  CurlReq::setup();
+  std::cout << "runs" << std::endl;
+  if(argc == 1){
+              std::cout << "succesfull compalation, no parameter" << std::endl;
+              std::thread InGameTrades(OrderHandling::EElogChecking);
+              InGameTrades.join();
+  }
+  else {
+      std::ofstream del("out.log", std::ios::trunc);
+      del.close();
+      OrderHandling::DeleteOrder(argv[1]);
+      json items = CurlReq::q.Add([]{ return
+  CurlReq::__GET("https://api.warframe.market/v2/items");}).get(); for(json
+  item: items["data"]){ auto trade = Sorting::ValidTrade(item["slug"],
+  item["tags"].get<std::vector<std::string>>(), true); if(trade.good_trade){
+              std::string id = item["id"].get<std::string>();
+              OrderHandling::PostOrder(argv[1], id, tradeType::buy, trade);
+          }
+      }
+      CurlReq::disconnect();
+  }*/
+  return 0;
 }
